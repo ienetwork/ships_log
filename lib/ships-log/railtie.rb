6168169cbe74c066aebb585764855215d722e545
Brandon Beeks <brandon@beeks.me>
2020-09-25 15:35:36 -0600

    Add sbli as e-ticket option

--- lib/ships-log/railtie.rb
+++ lib/ships-log/railtie.rb
@@ -439,6 +439,12 @@ class ShipsLog < Rails::Railtie
         :products => ["BgaQuoter", "MobileApp"]
       },
       {
+        :value => "sbli",
+        :label => "SBLI",
+        :group => "E-Ticket",
+        :products => ["BgaQuoter", "MobileApp"]
+      },
+      {
         :value => "typeform",
         :label => "TypeForm",
         :group => "Form",
