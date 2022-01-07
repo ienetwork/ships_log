01efda454862f021f37189e54805570755a70fa5
Brandon Beeks <brandon@beeks.me>
2022-01-06 19:11:08 -0700

    v0.3.9: Update firelight quote action name

--- lib/ships-log/railtie.rb
+++ lib/ships-log/railtie.rb
@@ -451,15 +451,9 @@ class ShipsLog < Rails::Railtie
         :products => ["BgaQuoter", "MobileApp"]
       },
       {
-        :value => "firelight_eticket",
-        :label => "Firelight E-Ticket",
-        :group => "E-Ticket",
-        :products => ["BgaQuoter"]
-      },
-      {
-        :value => "firelight_eapp",
-        :label => "Firelight E-Application",
-        :group => "E-App",
+        :value => "firelight",
+        :label => "Firelight",
+        :group => "Integrations",
         :products => ["BgaQuoter"]
       },
       {
